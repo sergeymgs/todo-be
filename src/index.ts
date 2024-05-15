d3eec143b651fc3993a8b30037387d043b4b94ec
Sergey <sergey@example.com>
2024-05-15 12:01:46 +0300

    get method + database

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 import express, { response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import { tasks } from './db/schema';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+if (!PORT) {
+    console.log("Port not defined in the .env file.");
+    process.exit(0);
+}
 
-app.get('/api/tasks', (request, response) => {
-    response.send([
-        {id: 1, title: 'Первый таск', isDone: true},
-        {id: 2, title: 'Второй таск', isDone: true},
-        {id: 3, title: 'Третий таск', isDone: false},
-        {id: 4, title: 'Четвертый таск', isDone: true},
-        {id: 5, title: 'Пятый таск', isDone: false},
-    ]);
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!DATABASE_URL) {
+    console.log("Missing DATABASE_URL setting in .env file.");
+    process.exit(0);
+}
+
+app.get('/api/tasks', async (request, response) => {
+    const queryClient = postgres(DATABASE_URL);
+    const db = drizzle(queryClient);
+    const result = await db.select().from(tasks);
+    response.send(result);
 });
 
 app.listen(PORT, () => {
